c3b7e602be2790def2b513ebe8170bd898a1a5c9
ivanoung <ivanoung@gmail.com>
2018-10-20 13:32:19 +0800

    hello world is running with ts, and modulated

---
+++ src/Page/Dashboard/dashboard.tsx
@@ -0,0 +1,31 @@
+// Importing modules
+import * as React from "react";
+
+// Importing styling and static assets
+import "./dashboard.css";
+
+// Importing UI components
+
+
+// for redir
+// import * as History from "history";
+// import { withRouter } from "react-router";
+
+
+export default class PureCurrentOrders extends React.Component {
+
+
+//   public componentDidMount() {
+
+//   }
+
+  public render() {
+    return (
+      <div className="desktop-page-container">
+      <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+
+  <Router>
+    <Route component={App} />
+  </Router>,
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();

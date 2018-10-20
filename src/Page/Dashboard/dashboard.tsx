793bb1732a227235d80945ed4e8dacab032c8749
ivanoung <ivanoung@gmail.com>
2018-10-20 13:59:27 +0800

    trying to get vega lite

--- src/Page/Dashboard/dashboard.tsx
+++ src/Page/Dashboard/dashboard.tsx
@@ -1,5 +1,7 @@
 // Importing modules
 import * as React from "react";
+import * as VegaLite from 'react-vega-lite';  
+
 
 // Importing styling and static assets
 import "./dashboard.css";
@@ -7,22 +9,35 @@ import "./dashboard.css";
 // Importing UI components
 
 
+
 // for redir
 // import * as History from "history";
 // import { withRouter } from "react-router";
 
+const spec = {
+  "description": "A simple bar chart with embedded data.",
+  "encoding": {
+    "x": {"field": "a", "type": "ordinal"},
+    "y": {"field": "b", "type": "quantitative"}
+  },
+  "mark": "bar"
+};
 
-export default class PureCurrentOrders extends React.Component {
+const barData = {
+  "values": [
+    {"a": "A","b": 20}, {"a": "B","b": 34}, {"a": "C","b": 55},
+    {"a": "D","b": 19}, {"a": "E","b": 40}, {"a": "F","b": 34},
+    {"a": "G","b": 91}, {"a": "H","b": 78}, {"a": "I","b": 25}
+  ]
+};
 
+export default class PureCurrentOrders extends React.Component   {
 
-//   public componentDidMount() {
-
-//   }
-
+  
   public render() {
     return (
       <div className="desktop-page-container">
-      <h1>Hello world</h1>
+      <VegaLite spec={spec} data={barData} />
       </div>
     );
   }

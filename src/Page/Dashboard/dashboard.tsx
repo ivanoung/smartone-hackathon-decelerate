13e765525ee95294b0e744b5c2cbb1c94cf1bd64
Seamus Yeo <seamus.y2016@gmail.com>
2018-10-20 18:54:46 +0800

    Uncommented all files

--- src/Page/Dashboard/dashboard.tsx
+++ src/Page/Dashboard/dashboard.tsx
@@ -1,40 +1,38 @@
-// // Importing modules
-// import * as React from "react";
-// import * as VegaLite from 'react-vega-lite';  
+// Importing modules
+import * as React from "react";
+import * as VegaLite from 'react-vega-lite';  
 
-// // Importing styling and static assets
-// import "./dashboard.css";
+// Importing styling and static assets
+import "./dashboard.css";
 
-// // Importing UI components
+// Importing UI components
 
 
-// const spec = {
-//   "description": "A simple bar chart with embedded data.",
-//   "encoding": {
-//     "x": {"field": "a", "type": "ordinal"},
-//     "y": {"field": "b", "type": "quantitative"}
-//   },
-//   "mark": "bar"
-// };
+const spec = {
+  "description": "A simple bar chart with embedded data.",
+  "encoding": {
+    "x": {"field": "a", "type": "ordinal"},
+    "y": {"field": "b", "type": "quantitative"}
+  },
+  "mark": "bar"
+};
 
-// const barData = {
-//   "values": [
-//     {"a": "A","b": 20}, {"a": "B","b": 34}, {"a": "C","b": 55},
-//     {"a": "D","b": 19}, {"a": "E","b": 40}, {"a": "F","b": 34},
-//     {"a": "G","b": 91}, {"a": "H","b": 78}, {"a": "I","b": 25}
-//   ]
-// };
+const barData = {
+  "values": [
+    {"a": "A","b": 20}, {"a": "B","b": 34}, {"a": "C","b": 55},
+    {"a": "D","b": 19}, {"a": "E","b": 40}, {"a": "F","b": 34},
+    {"a": "G","b": 91}, {"a": "H","b": 78}, {"a": "I","b": 25}
+  ]
+};
 
-// export default class PureCurrentOrders extends React.Component   {
+export default class PureCurrentOrders extends React.Component   {
 
   
-//   public render() {
-//     return (
-//       <div className="desktop-page-container">
-//       <VegaLite spec={spec} data={barData} />
-//       </div>
-//     );
-//   }
-// }
-
-
+  public render() {
+    return (
+      <div className="desktop-page-container">
+      <VegaLite spec={spec} data={barData} />
+      </div>
+    );
+  }
+}
